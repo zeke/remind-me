960e573ee7304528ae9c14332c93c55557100f21
Zeke Sikelianos <zeke@sikelianos.com>
2015-08-31 23:30:27 -0500

    handle unrecognized actions

--- index.js
+++ index.js
@@ -3,22 +3,36 @@
 const fs = require('fs')
 const parse = require('./lib/parse')
 const strftime = require('prettydate').strftime
-const relative = require('relative-date')
+const usage = fs.readFileSync('./usage.txt', 'utf8')
+// const relative = require('relative-date')
 
 if (!process.parent) {
   var args = process.argv.slice(2)
 
+
   if (!args.length) {
-    console.log(fs.readFileSync('./usage.txt', 'utf8'))
+    console.log(usage)
     process.exit()
   }
 
-  var parsed = parse(args.join(' '))
+  var action = args[0]
 
-  var task = parsed.task
-  var day = strftime(parsed.time, '%A, %B%e')
-  var hour = strftime(parsed.time, '%l:%M %P')
-
-  console.log(`Ok, I'll remind you to "${task}" on ${day} at ${hour}`)
+  switch(action) {
+    case 'agent':
+      require('child_process').execSync('say i am the agent')
+      // Pending: parse the task list and look for
+      // stuff that's supposed to happen this minute
+      break
+    case 'me':
+      var parsed = parse(args.join(' '))
+      var task = parsed.task
+      var day = strftime(parsed.time, '%A, %B%e')
+      var hour = strftime(parsed.time, '%l:%M %P')
+      console.log(`Ok, I'll remind you to "${task}" on ${day} at ${hour}`)
+      break
+    default:
+      console.log(`Unrecognized command "${action}"\n`)
+      console.log(usage)
+  }
 
 }

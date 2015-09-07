f5d1edd6a85156114b3e7dbfe3dbca1bfbb6d0fb
Zeke Sikelianos <zeke@sikelianos.com>
2015-09-06 20:24:04 -0500

    create notifiers for desktop, twilio, and slack

--- index.js
+++ index.js
@@ -1,39 +1,36 @@
 #!/usr/bin/env node
 
 const fs = require('fs')
-const strftime = require('prettydate').strftime
-// const relative = require('relative-date')
-const usage = fs.readFileSync('./usage.txt', 'utf8')
-const Store = require('./lib/store')
-const Reminder = require('./lib/reminder')
 
-if (!process.parent) {
-  var args = process.argv.slice(2)
+const usage = fs.readFileSync(__dirname + '/lib/usage.txt', 'utf8')
+const Store = require(__dirname + '/lib/store')
+const Reminder = require(__dirname + '/lib/reminder')
+const args = process.argv.slice(2)
 
-  if (!args.length) {
+if (!args.length) {
+  console.log(usage)
+  process.exit()
+}
+
+const action = args[0]
+
+switch (action) {
+  case 'sweep':
+    require('child_process').execSync('echo I am here to sweep')
+    var store = new Store()
+    store.sweep()
+    break
+  case 'me':
+    var store = new Store()
+    var reminder = new Reminder(args.join(' '))
+    store.add(reminder)
+    console.log(reminder.summary)
+    break
+  case 'edit':
+    var store = new Store()
+    require('child_process').execSync(`$EDITOR ${store.file}`)
+    break
+  default:
+    console.log(`Unrecognized command "${action}"\n`)
     console.log(usage)
-    process.exit()
-  }
-
-  var action = args[0]
-
-  switch (action) {
-    case 'agent':
-      require('child_process').execSync('say i am the agent')
-      var store = new Store()
-      store.sweep()
-      // Pending: parse the task list and look for
-      // stuff that's supposed to happen this minute
-      break
-    case 'me':
-      var store = new Store()
-      var reminder = new Reminder(args.join(' '))
-      store.add(reminder)
-      console.log(reminder.summary)
-      break
-    default:
-      console.log(`Unrecognized command "${action}"\n`)
-      console.log(usage)
-  }
-
 }
